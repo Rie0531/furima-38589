95745bc3bbf0c8ca33b6758c37a465e5594f3fe2
Rie0531 <113961502+Rie0531@users.noreply.github.com>
2023-01-30 15:03:55 +0900

    Userモデルの単体テストコードの修正(3)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   
   with_options presence: true do
     validates :nickname, :birthday
-    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
     validates :read_last, :read_first, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
   end
   validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
